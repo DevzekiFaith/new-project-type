5e8ff5230e3fb724bc6cf527b27b603d04526b72
Dzek <142034720+DevzekiFaith@users.noreply.github.com>
2024-05-23 03:29:30 +0100

    secod commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,52 @@
 import Image from "next/image";
 
 export default function Home() {
-
   // DECLARE THE INTERFACE OF PROPERTIES//
 
-  interface Vehicle  {
-    make: String,
-    model: string,
-    year: number,
-    start(): void; 
-    }
-
-    // CREATE A NEW VARIABLE A OBJECT TO HOLD THE INTERFACE PROPERTIES//
-
-    const myVehicle: Vehicle = {
-      make: "Toyota",
-      model: "Lexus",
-      year: 2024,
-      start() {
-          console.log("Engine started")
-      },
-
-    };
-
-    myVehicle.start();
-
-   
-
-    class car implements Vehicle  {
-      make: string;
-      model: string;
-      year: number;
-
-
-      constructor(make: string, model: string, year: number ) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-      }
-      start(){
-        console.log("Car Engine started")
-      }
-
-    }
-
-    const myCar = new car ("Bmw","Bmw", 2024);
-    myCar.start();
-
-
-    return (
-      <main className=" bg-green-500">
-        <div>
-          <h1 className="text-pink-500">This the Vehicle method for the car</h1>
-        </div>
-      </main>
-    );
+  interface Vehicle {
+    make: String;
+    model: string;
+    year: number;
+    start(): void;
   }
 
+  // CREATE A NEW VARIABLE A OBJECT TO HOLD THE INTERFACE PROPERTIES//
+
+  const myVehicle: Vehicle = {
+    make: "Toyota",
+    model: "Lexus",
+    year: 2024,
+    start() {
+      console.log("Engine started");
+    },
+  };
+
+  myVehicle.start();
+
+  class car implements Vehicle {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+    start() {
+      console.log("Car Engine started");
+    }
+  }
+
+  const myCar = new car("Bmw", "Bmw", 2024);
+  myCar.start();
+
+
+  return (
+    <main className=" bg-green-500">
+      <div>
+        <h1 className="text-pink-500">This the Vehicle method for the car</h1>
+      </div>
+    </main>
+  );
+}
---
+++ src/component/stateWork/StateWork.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StateWork = (): void => {
+  const Greeting = ({ name }: {name: string}) => {
+    return <div>Hello, {name}!</div>;
+
+    
+
+    return <div></div>;
+  };
+};
+export default StateWork;
